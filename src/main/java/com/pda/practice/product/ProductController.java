bbe82cde25882a976dbd81cb8f8e9ca2d2a15ec7
bkkmw <roboticts49@gmail.com>
2024-05-07 17:50:33 +0900

    remove: unnecessary import codes

--- src/main/java/com/pda/practice/product/ProductController.java
+++ src/main/java/com/pda/practice/product/ProductController.java
@@ -3,7 +3,6 @@ package com.pda.practice.product;
 import com.pda.practice.exception.ValidatorException;
 import com.pda.practice.utils.ValidationUtil;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 @RestController
 @RequiredArgsConstructor
