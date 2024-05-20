4b5874e9516f6e56515df89e5acb552a60b6745d
bkkmw <roboticts49@gmail.com>
2024-05-21 08:52:39 +0900

    fix: typo

--- src/main/java/com/pda/practice/test/TestController.java
+++ src/main/java/com/pda/practice/test/TestController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,7 @@ public class TestController {
         return new ResponseEntity<>(reqBody.getTestValue(), HttpStatus.OK);
     }
 
-    @PostMapping("lower-dot")
+    @PostMapping("/lower-dot")
     public ResponseEntity<String> lowerDot(@RequestBody TestDto.LowerDotCase reqBody) {
         log.info("Req body : {}", reqBody.getTestValue());
         return new ResponseEntity<>(reqBody.getTestValue(), HttpStatus.OK);
