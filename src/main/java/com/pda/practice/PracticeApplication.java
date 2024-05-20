cc9017e76983681fa9a28471c755c5b5d7a46c9f
bkkmw <roboticts49@gmail.com>
2024-05-21 08:32:57 +0900

    feat: add filter configuration

--- src/main/java/com/pda/practice/PracticeApplication.java
+++ src/main/java/com/pda/practice/PracticeApplication.java
@@ -2,8 +2,10 @@ package com.pda.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class PracticeApplication {
 
 	public static void main(String[] args) {
