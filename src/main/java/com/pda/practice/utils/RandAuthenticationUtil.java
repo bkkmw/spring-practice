9ffbcdc35acf62c0e3bf1faf1f2ce539582ad57b
bkkmw <roboticts49@gmail.com>
2024-05-22 15:16:51 +0900

    feat: add Auth util prototype

---
+++ src/main/java/com/pda/practice/utils/RandAuthenticationUtil.java
@@ -0,0 +1,12 @@
+package com.pda.practice.utils;
+
+import com.pda.practice.entity.User;
+
+public class RandAuthenticationUtil {
+
+    public static String[] createToken(User user) {
+        return new String[] {
+                "NIY"
+        };
+    }
+}
