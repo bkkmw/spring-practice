06e65c51fca33d6c9ea1a80b520b8a1cf4fb4065
bkkmw <roboticts49@gmail.com>
2024-05-23 09:49:17 +0900

    feat: add product and category schema

--- schema.sql
+++ schema.sql
@@ -9,6 +9,28 @@ CREATE TABLE `user` (
     `name` VARCHAR(20) NOT NULL,
     `email` VARCHAR(40) NOT NULL,
     `contact` CHAR(13) NOT NULL,
-    
+
     PRIMARY KEY(`id`)
+);
+
+DROP TABLE IF EXISTS `product`;
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+	`id` SMALLINT UNSIGNED UNIQUE,
+    `name` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY(`id`)
+);
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+	`id` MEDIUMINT UNSIGNED UNIQUE AUTO_INCREMENT,
+    `category_id` SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
+    `name` VARCHAR(30) NOT NULL,
+    `desc` TEXT NOT NULL,
+    `summary` VARCHAR(255) NOT NULL,
+    `price` INT NOT NULL,
+
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`category_id`) REFERENCES `category`(`id`)
 );
